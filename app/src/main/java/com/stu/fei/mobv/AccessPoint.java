10a349144b62500b936d5e66a42c92f01e221014
Miroslav Adamec <miroslav.adamec@invia.cz>
2016-11-14 13:38:38 +0100

    AccessPoint object created

---
+++ app/src/main/java/com/stu/fei/mobv/AccessPoint.java
@@ -0,0 +1,15 @@
+package com.stu.fei.mobv;
+
+/**
+ * Created by miroslav.adamec on 14.11.2016.
+ */
+public class AccessPoint {
+    public String ssid;
+    public String bssid;
+    public String capabilities;
+    public String level;
+    public String frequency;
+    public String timestamp;
+    public String distance;
+    public String distanceSd;
+}
