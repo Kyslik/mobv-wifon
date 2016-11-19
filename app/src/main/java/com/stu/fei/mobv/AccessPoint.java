cb61d5d6f1a11775f69ab6c6f0db94de701ef30a
Roman Moravcik <roman.moravcik@mileware.com>
2016-11-19 19:12:44 +0100

    Filling AccessPoint with scanned data

--- app/src/main/java/com/stu/fei/mobv/AccessPoint.java
+++ app/src/main/java/com/stu/fei/mobv/AccessPoint.java
@@ -1,5 +1,7 @@
 package com.stu.fei.mobv;
 
+import android.net.wifi.ScanResult;
+
 /**
  * Created by miroslav.adamec on 14.11.2016.
  */
@@ -12,4 +14,16 @@ public class AccessPoint {
     public String timestamp;
     public String distance;
     public String distanceSd;
+
+    public static AccessPoint createNew(ScanResult scannedAP) {
+        AccessPoint accessPoint = new AccessPoint();
+        accessPoint.ssid = scannedAP.SSID;
+        accessPoint.bssid = scannedAP.BSSID;
+        accessPoint.capabilities = scannedAP.capabilities;
+        accessPoint.level = Integer.toString(scannedAP.level);
+        accessPoint.frequency = Integer.toString(scannedAP.frequency);
+        accessPoint.timestamp = Long.toString(scannedAP.timestamp);
+
+        return accessPoint;
+    }
 }
--- app/src/main/java/com/stu/fei/mobv/MainActivity.java
+++ app/src/main/java/com/stu/fei/mobv/MainActivity.java
@@ -21,25 +21,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<AccessPoint> testArr = new ArrayList<AccessPoint>();
-        AccessPoint temp1 = new AccessPoint();
-        temp1.bssid="nejakeBssid";
-        temp1.ssid="nejakeSsid";
-        testArr.add(temp1);
-        testArr.add(temp1);
-        testArr.add(temp1);
-
-        //List<String> testArr = new ArrayList<String>();
-
-        /*WifiManager mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiManager mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         mainWifi.startScan();
         List<ScanResult> wifiList =mainWifi.getScanResults();
-        for(int i = 0; i < wifiList.size(); i++){
-            testArr.add((wifiList.get(i)).toString());
-        }*/
 
+        List<AccessPoint> accessPoints = new ArrayList<AccessPoint>();
+         for (int i = 0; i < wifiList.size(); ++i) {
+            accessPoints.add(AccessPoint.createNew(wifiList.get(i)));
+        }
 
-        ListAdapter wifiAdapter = new AddWifiArrayAdapter(this, testArr);
+        ListAdapter wifiAdapter = new AddWifiArrayAdapter(this, accessPoints);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(wifiAdapter);
     }
